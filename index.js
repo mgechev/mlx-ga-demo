5a7f9434bb88f489db07708ea3c1a1b7c72ceef1
mgechev <mgechev@gmail.com>
2018-04-02 19:57:18 -0700

    refactor: use google oauth2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { fetch } = require('@mlx/ga');
 const { parseRoutes, ProjectType } = require('@mlx/parser');
 const { writeFileSync } = require('fs');
+const { auth } = require('google-oauth2-node');
+const { google } = require('googleapis');
+
+const ClientID = '329457372673-hda3mp2vghisfobn213jpj8ck1uohi2d.apps.googleusercontent.com';
+const ClientSecret = '4camaoQPOz9edR-Oz19vg-lN';
+const Scope = 'https://www.googleapis.com/auth/analytics.readonly';
 
 const key = require('./credentials.json');
 const viewId = '128035004';
@@ -10,16 +16,31 @@ const applicationRoutes = parseRoutes(
   ProjectType.Angular
 );
 
-fetch({
-  key,
-  viewId,
-  period: {
-    startDate: new Date('2016-1-1'),
-    endDate: new Date('2018-2-24')
-  },
-  formatter: r => r.replace('/app', ''),
-  routes: applicationRoutes.map(f => f.path)
-}).then(g => {
-  writeFileSync('data.json', JSON.stringify(g, null, 2));
+auth({
+  clientId: ClientID,
+  clientSecret: ClientSecret,
+  scope: Scope
+}).then(getData, err => {
+  console.error(err);
 });
 
+const getData = ({ access_token, refresh_token }) => {
+  const oauth2Client = new google.auth.OAuth2();
+  oauth2Client.setCredentials({
+    access_token,
+    refresh_token
+  });
+
+  fetch({
+    auth: oauth2Client,
+    viewId,
+    period: {
+      startDate: new Date('2016-1-1'),
+      endDate: new Date('2018-2-24')
+    },
+    formatter: r => r.replace('/app', ''),
+    routes: applicationRoutes.map(f => f.path)
+  }).then(g => {
+    writeFileSync('data.json', JSON.stringify(g, null, 2));
+  });
+};
